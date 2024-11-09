926e8b5617be7ac937402dec455916da21806b4e
AndrexYT <andrexyt2012@gmail.com>
2024-11-09 20:17:49 +0100

    Inputs and better logs

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,15 @@ use reqwest;
 use sha1::{Sha1, Digest};
 use std::process::Command;
 use crossterm::style::{Color, SetForegroundColor, ResetColor};
+use std::io;
 
-const USERNAME: &str = "ArduWallet";
-const MINING_KEY: &str = "";
-const USE_LOWER_DIFF: bool = false;
-const SOFTWARE: &str = "Official PC Miner 3.5"; // ['Official PC Miner 3.5', 'Official ESP8266 Miner 3.5', 'Official ESP32 Miner 3.5', 'Duino-Coin AVR Miner 4.2'
-const IDENTIFIER: &str = "Rust1"; // RIG1 - RPi
-const CHIP_ID: &str = "1";
+const ASK_DATA: bool = false;
+const DEFAULT_USERNAME: &str = "ArduWallet";
+const DEFAULT_MINING_KEY: &str = "";
+const DEFAULT_USE_LOWER_DIFF: bool = false;
+const DEFAULT_SOFTWARE: &str = "Official PC Miner 3.5"; // ['Official PC Miner 3.5', 'Official ESP8266 Miner 3.5', 'Official ESP32 Miner 3.5', 'Duino-Coin AVR Miner 4.2'
+const DEFAULT_IDENTIFIER: &str = "Rust1"; // RIG1 - RPi
+const DEFAULT_CHIP_ID: &str = "1";
 
 fn fetch_pools() -> (String, u16) {
     loop {
@@ -74,6 +76,57 @@ fn get_cpu_temp() -> String {
 }
 
 fn main() {
+    let (username, mining_key, identifier, chip_id, software, use_lower_diff) = if ASK_DATA {
+        let mut input = String::new();
+        println!("Enter Username[ArduWallet]:");
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+        let mut username = input.trim().to_string();
+        if username == "" {
+            username = DEFAULT_USERNAME.to_string();
+        }
+
+        input.clear();
+        println!("Enter Mining Key[]:");
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+        let mining_key = input.trim().to_string();
+
+        input.clear();
+        println!("Use lower difficulty[y/n]:");
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+        let mut use_lower_diff = input.trim().to_string();
+        if use_lower_diff == "" {
+            use_lower_diff = DEFAULT_USE_LOWER_DIFF.to_string();
+        }
+
+        input.clear();
+        println!("Enter Software[Official PC Miner 3.5]:");
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+        let mut software = input.trim().to_string();
+        if software == "" {
+            software = DEFAULT_SOFTWARE.to_string();
+        }
+
+        input.clear();
+        println!("Enter Identifier[RustMiner]:");
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+        let mut identifier = input.trim().to_string();
+        if identifier == "" {
+            identifier = DEFAULT_IDENTIFIER.to_string();
+        }
+
+        input.clear();
+        println!("Enter Chip ID[1]:");
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+        let mut chip_id = input.trim().to_string();
+        if chip_id == "" {
+            chip_id = DEFAULT_CHIP_ID.to_string();
+        }
+
+        (username, mining_key, identifier, chip_id, software, use_lower_diff)
+    } else {
+        (DEFAULT_USERNAME.to_string(), DEFAULT_MINING_KEY.to_string(), DEFAULT_IDENTIFIER.to_string(), DEFAULT_CHIP_ID.to_string(), DEFAULT_SOFTWARE.to_string(), DEFAULT_USE_LOWER_DIFF.to_string())
+    };
+
     if get_cpu_temp() == "0" {
         println!("{}Warning: {}Failed to retrieve CPU temperature, try running the script as administrator...", SetForegroundColor(Color::Yellow), ResetColor);
     }
@@ -83,35 +136,38 @@ fn main() {
         };
 
         // Socket
-        let mut soc = TcpStream::connect(format!("{}:{}", node_address, node_port)).expect("Failed to connect to server");
+        let mut socket = TcpStream::connect(format!("{}:{}", node_address, node_port)).expect("Failed to connect to server");
         let mut buffer = [0; 100];
-        soc.read(&mut buffer).expect("Failed to read from server");
+        socket.read(&mut buffer).expect("Failed to read from server");
         println!("{}Server Version: {}{}", SetForegroundColor(Color::Yellow), str::from_utf8(&buffer).unwrap().replace("\n", ""), ResetColor);
-        println!("{}Logged in as: {}{}", SetForegroundColor(Color::Yellow), if IDENTIFIER.is_empty() { SOFTWARE } else { IDENTIFIER }, ResetColor);
+        println!("{}Logged in as: {}{}", SetForegroundColor(Color::Yellow), if identifier.is_empty() { &software } else { &identifier }, ResetColor);
+        println!("{}Account: {}{}", SetForegroundColor(Color::Yellow), username, ResetColor);
+        println!("{}Mining key: {}{}", SetForegroundColor(Color::Yellow), mining_key, ResetColor);
 
         // Mine
         loop {
-            let difficulty = if USE_LOWER_DIFF { "LOW" } else { "MEDIUM" };
-            let job_request = format!("JOB,{},{},{},{}@{}\n", USERNAME, difficulty, MINING_KEY, get_cpu_temp(), "0");
-            soc.write_all(job_request.as_bytes()).expect("Failed to send job request");
+            let difficulty = if use_lower_diff == "y" { "LOW" } else { "MEDIUM" };
+            let job_request = format!("JOB,{},{},{},{}@{}\n", username, difficulty, mining_key, get_cpu_temp(), "0");
+            socket.write_all(job_request.as_bytes()).expect("Failed to send job request");
 
             // Receive job
             let mut job_buffer = [0; 1024];
-            let bytes_read = soc.read(&mut job_buffer).expect("Failed to read job from server");
+            let bytes_read = socket.read(&mut job_buffer).expect("Failed to read job from server");
             let job = str::from_utf8(&job_buffer[..bytes_read]).unwrap().trim_end().split(',').collect::<Vec<&str>>();
 
             let (result, hashrate) = solve_job(job.clone());
 
             // Send result
-            let result_message = format!("{},{},{},{},DUCOID{}", result, hashrate, SOFTWARE, IDENTIFIER, CHIP_ID);
-            soc.write_all(result_message.as_bytes()).expect("Failed to send result");
+            let result_message = format!("{},{},{},{},DUCOID{}", result, hashrate, software, identifier, chip_id);
+            socket.write_all(result_message.as_bytes()).expect("Failed to send result");
 
             // Get feedback
             let mut feedback_buffer = [0; 1024];
-            let bytes_read = soc.read(&mut feedback_buffer).expect("Failed to read feedback from server");
+            let bytes_read = socket.read(&mut feedback_buffer).expect("Failed to read feedback from server");
             let feedback = str::from_utf8(&feedback_buffer[..bytes_read]).unwrap().trim_end();
 
             // Process feedback
+            let target_width: i32 = 30;  // Adjust this value to set the desired alignment for "|"
             match feedback {
                 "GOOD" => {
                     let hashrate_display = if hashrate >= 1_000_000.0 {
@@ -119,7 +175,21 @@ fn main() {
                     } else {
                         format!("{} kH/s", (hashrate / 1000.0).round() as u64)
                     };
-                    println!("{}Accepted share {} | Hashrate {} | Difficulty {} | Motherboard temp {}°C{}", SetForegroundColor(Color::Green), result, hashrate_display, job[2], get_cpu_temp(), ResetColor);
+
+                    let result_str = result.to_string();
+                    let spaces_after_result = target_width.saturating_sub((result_str.len() + "Accepted share ".len()).try_into().unwrap());
+
+                    println!(
+                        "{}Accepted share {}{}{}| Hashrate {} | Difficulty {} | Motherboard temp {}°C{}", 
+                        SetForegroundColor(Color::Green),
+                        SetForegroundColor(Color::Cyan),
+                        result_str,
+                        " ".repeat(spaces_after_result.try_into().unwrap()),
+                        hashrate_display, 
+                        job[2], 
+                        get_cpu_temp(), 
+                        ResetColor
+                    );
                 }
                 "BAD" => {
                     let hashrate_display = if hashrate >= 1_000_000.0 {
@@ -127,7 +197,21 @@ fn main() {
                     } else {
                         format!("{} kH/s", (hashrate / 1000.0).round() as u64)
                     };
-                    println!("{}Rejected share {} | Hashrate {} | Difficulty {} | Motherboard temp {}°C{}", SetForegroundColor(Color::Red), result, hashrate_display, job[2], get_cpu_temp(), ResetColor);
+
+                    let result_str = result.to_string();
+                    let spaces_after_result = target_width.saturating_sub((result_str.len() + "Rejected share ".len()).try_into().unwrap());
+
+                    println!(
+                        "{}Rejected share {}{}{}| Hashrate {} | Difficulty {} | Motherboard temp {}°C{}", 
+                        SetForegroundColor(Color::Red),
+                        SetForegroundColor(Color::Cyan),
+                        result_str,
+                        " ".repeat(spaces_after_result.try_into().unwrap()),
+                        hashrate_display, 
+                        job[2], 
+                        get_cpu_temp(), 
+                        ResetColor
+                    );
                 }
                 _ => {
                     let hashrate_display = if hashrate >= 1_000_000.0 {
@@ -135,9 +219,26 @@ fn main() {
                     } else {
                         format!("{} kH/s", (hashrate / 1000.0).round() as u64)
                     };
-                    println!("{}Malformed share: {} | {} | Hashrate {} | Difficulty {} | Motherboard temp {}°C{}", SetForegroundColor(Color::Red), feedback, result, hashrate_display, job[2], get_cpu_temp(), ResetColor);
+
+                    let result_str = result.to_string();
+                    let spaces_after_result = target_width.saturating_sub((result_str.len() + "Malformed share: ".len() + feedback.len()).try_into().unwrap());
+
+                    println!(
+                        "{}Malformed share: {}{}{}| {} | Hashrate {} | Difficulty {} | Motherboard temp {}°C{}", 
+                        SetForegroundColor(Color::Red),
+                        SetForegroundColor(Color::Cyan),
+                        feedback,
+                        result_str,
+                        " ".repeat(spaces_after_result.try_into().unwrap()),
+                        hashrate_display, 
+                        job[2], 
+                        get_cpu_temp(), 
+                        ResetColor
+                    );
                 }
             }
+
+
         }
     }
 }
